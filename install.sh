de27b948cdb595d91852c8786b51776b69ac1f4d
Chris Dutrow <chris.m.dutrow@gmail.com>
2020-09-09 19:03:42 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
-curl https://raw.githubusercontent.com/wg-dashboard/wg-dashboard/master/install_script.sh | sudo bash
+# curl https://raw.githubusercontent.com/wg-dashboard/wg-dashboard/master/install_script.sh | sudo bash
+curl https://raw.githubusercontent.com/DutrowLLC/wireguardserver/master/wireguardinstall.sh | sudo bash
 sudo systemctl enable wg-quick@wg0
 sudo ufw allow 3000
 echo "http://`curl -s "ifconfig.me"`:3000/createuser"
